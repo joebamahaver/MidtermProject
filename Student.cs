739af9b18ebed8fd7cb1743b90f35f39ae6ead2b
BuildTools <unconfigured@null.spigotmc.org>
2025-03-15 20:30:18 -0500

    dothatshit

--- Student.cs
+++ Student.cs
@@ -15,4 +15,51 @@ public class Student
         this.major=major;
         this.creditHours=creditHours;
     }
+    public int getCreditHours()
+    {
+        return this.creditHours;
+    }
+    public void calculateClassStatus(int creditHours)
+    {
+        if(creditHours<=29)
+        {
+            this.classStatus=classStatus.Freshman;
+        }
+        else if(creditHours<=59)
+        {
+            this.classStatus=classStatus.Sophomore;
+        }
+        else if(creditHours<=89)
+        {
+            this.classStatus=classStatus.Junior;
+        }
+        else if(creditHours>=90)
+        {
+            this.classStatus=classStatus.Senior;
+        }
+        else{
+            Console.WriteLine("Not an integer, calculatetatus method");
+            throw new("Not an integer, calculatestatus method");
+        }
+    }
+    public void calculateClassHours(int additionalHours)
+    {
+        int totalHours=(this.creditHours+additionalHours);
+        this.creditHours=totalHours;
+        calculateClassStatus(this.creditHours);
+    }
+    public void addTestScores(float testScore)
+    {
+        (this.testScores).Add(testScore);
+    }
+    public List<string> giveTestScores()
+    {
+        List<string> testScores=new List<string>();
+        int i=1;
+        foreach(float score in this.testScores)
+        {
+            testScores.Add($"Test{i}: {score}");
+        }
+        return testScores;
+    }
 }
--- meth.cs
+++ meth.cs
@@ -1,1 +1,33 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace MidtermProject;
+public class Methods
+{
+    public static classStatus calculateClassStatus(int creditHours)
+    {
+        if(creditHours<=29)
+        {
+            return classStatus.Freshman;
+        }
+        else if(creditHours<=59)
+        {
+            return classStatus.Sophomore;
+        }
+        else if(creditHours<=89)
+        {
+            return classStatus.Junior;
+        }
+        else if(creditHours>=90)
+        {
+            return classStatus.Senior;
+        }
+        else{
+            Console.WriteLine("Not an integer, getstatus method");
+            throw new("Not an integer, getstatus method");
+        }
+    }
+    public static void calculateClassHours(int additionalHours,Student student)
+    {
+        
+    }
+}
